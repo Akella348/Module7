76bbe032d89ca7e137dd766975ba1b4ddf269727
Akella348 <akella348@gmail.com>
2024-10-29 21:58:28 +0400

    2 iteration

--- module_7_3.py
+++ module_7_3.py
@@ -34,3 +34,29 @@ finder2 = WordsFinder('test_file.txt')
 print(finder2.get_all_words()) # Все слова
 print(finder2.find('TEXT')) # 3 слово по счёту
 print(finder2.count('teXT')) # 4 слова teXT в тексте всего
+
+team1_num = 5
+team2_num = 6
+score_1 = 40
+score_2 = 42
+team1_time = 1552.512
+team2_time = 2153.31451
+tasks_total = 82
+time_avg = 45.2
+if score_1 > score_2 or (score_1 == score_2 and team1_time > team2_time):
+    challenge_result = 'Победа команды Мастера кода!'
+elif score_1 < score_2 or (score_1 == score_2 and team1_time < team2_time):
+    challenge_result = 'Победа команды Волшебники данных!'
+else:
+    challenge_result = 'Ничья!'
+
+print("В команде Мастера кода участников: %d !" % team1_num) # Использование %
+print("Итого сегодня в командах участников: %d и %d !" % (team1_num, team2_num)) # Использование %
+
+print("Команда Волшебники данных решила задач: {} !".format(score_2)) # Использование format()
+print("Волшебники данных решили задачи за {:.1f} с !".format(team2_time)) # Использование format()
+
+print(f"Команды решили {score_1} и {score_2} задач.") # Использование f-строк
+print(f"Результат битвы: {challenge_result}") # Использование f-строк
+
+print(f"Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!.") # Использование f-строк
