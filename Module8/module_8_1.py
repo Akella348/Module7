94eb2328522f9f60e7d4242ec8454abafb7842b5
Akella348 <akella348@gmail.com>
2024-11-03 21:35:29 +0400

    1 iteration

---
+++ Module8/module_8_1.py
@@ -0,0 +1,8 @@
+def add_everything_up(a, b):
+    try:
+        return round(a + b, 3) if isinstance(a, float) or isinstance(b, float) else a + b
+    except TypeError:
+        return str(a) + str(b)
+print(add_everything_up(123.456 , 'строка'))
+print(add_everything_up('яблоко' , 4215))
+print(add_everything_up(123.456 , 7))
