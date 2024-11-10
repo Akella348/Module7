9282d75eb5c2c80995e862944396daec6cd04e8e
Akella348 <akella348@gmail.com>
2024-11-11 00:59:43 +0400

    1 iteration

--- Module10/module_10_3.py
+++ Module10/module_10_3.py
@@ -4,32 +4,41 @@ class Bank(threading.Thread):
     def __init__(self):
         self.balance = 0
         self.lock = threading.Lock()
+
     def deposit(self):
         count = 100
         while count > 0:
-            count -= 1
-            some_number = random.randint(50, 500)
-            with self.lock:
+            if self.lock.locked():
+                count -= 1
+                some_number = random.randint(50, 500)
                 self.balance += some_number
                 print(f"Пополнение: {some_number}. Баланс: {self.balance}")
-                if self.balance >= 500 and not self.lock.locked():
+                if self.balance >= 500 and self.lock.locked():
                     self.lock.release()
-
+            else:
+                count -= 1
             time.sleep(0.001)
+
+
+
+
     def take(self):
         count = 100
         while count > 0:
-            count -= 1
-            some_number = random.randint(50, 500)
-            print(f"Запрос на {some_number}")
-            with self.lock:
+            if not self.lock.locked():
+                count -= 1
+                some_number = random.randint(50, 500)
+                print(f"Запрос на {some_number}")
                 if some_number <= self.balance:
                     self.balance -= some_number
                     print(f"Снятие: {some_number}. Баланс: {self.balance}")
                 else:
                     print("Запрос отклонён, недостаточно средств")
                     self.lock.acquire()
+            else:
+                count -= 1
             time.sleep(0.001)
+
 
 bank = Bank()
 th1 = threading.Thread(target=Bank.deposit, args=(bank,))
